00aea24104a3e4eb10e5c51f3d4a7bc89c944b94
Paulo Henrique de Barros Brandão <paulobrandaohbb@gmail.com>
2026-01-16 12:08:39 -0300

    atualização no controller

---
+++ src/funcionarios/controller/funcionarios.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Funcionarios } from '../entities/funcionarios.entity';
+import { FuncionariosService } from '../services/funcionarios.service';
+
+
+@Controller('/funcionarios')
+export class FuncionariosController {
+  constructor(private readonly funcionariosService: FuncionariosService) {}
+  }
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Funcionarios[]> {
+    return this.funcionariosService.findAll();
+  }
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Funcionarios> {
+    return this.funcionariosService.findById(id);
+
+  @Get('/cpf/:cpf')
+  @HttpCode(HttpStatus.OK)
+  findByCpf(@Param('cpf') cpf: string,): Promise<Funcionarios> {
+    return this.funcionariosService.findByCpf(cpf);
+  }
+  @Get('/nome/:nome')
+  @HttpCode(HttpStatus.OK)
+  findByAllTitulo(@Param('nome') nome: string): Promise<Funcionarios[]> {
+    return this.funcionariosService.findByIdTitulo(nome);
+  }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() funcionarios: Funcionaarios): Promise<Funcionarios> {
+    return this.funcionariosService.create(funcionarios);
+  }
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() funcionarios: Funcionarios): Promise<Funcionarios> {
+    return this.funcionariosService.update(funcionarios);
+  }
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.funcionariosService.delete(id);
+  }
+}
