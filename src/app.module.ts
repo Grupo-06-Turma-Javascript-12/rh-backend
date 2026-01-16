dbf2eb7f4df20bb8fc26c0cee65a64cef018e96f
Thays Peixoto <thabysilva12@gmail.com>
2026-01-16 11:00:32 -0300

    realizada alterações do modulo para banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Funcionarios } from './funcionarios/entities/funcionarios.entity';
+// import { FuncionariosModule } from './funcionarios/funcionarios.module';
+// import { Tema } from './tema/entities/tema.entity';
+// import { TemaModule } from './tema/tema.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_rh_backend',
+      entities: [Funcionarios],
+      synchronize: true,
+      logging: true,
+    }),
+      FuncionarioModule,
+      TemaModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
